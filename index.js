160b4703d550774583ef3ebe6dff21c4fbe29fc5
TheMalikShow <73982059+TheMalikShow@users.noreply.github.com>
2023-02-02 11:48:05 +0500

    months

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 //nested loops
 
 for (var year = 2023 ; year < 2025 ; year ++){
-    console.log(year)
+    console.log(year);
+    for(var month=6 ; month < 9 ; month++){
+        console.log("....",month)
+    }
 }
