e975a6043c5ffba03d8597ad19f429a3ea98143f
TheMalikShow <73982059+TheMalikShow@users.noreply.github.com>
2023-02-06 20:38:35 +0500

    done

--- LittleLemon.js
+++ LittleLemon.js
@@ -21,29 +21,34 @@ const tax = 1.20;
 
 // Implement getPrices()
 function getPrices(taxBoolean) {
-    for (i=0 ; i=dishData.length; i++){
-        var finalPrice;
-        if(taxBoolean===true){
+    for(let i = 0; i < dishData.length; i++){
+        let finalPrice
+        if(taxBoolean === true){
             finalPrice = dishData[i]['price'] * tax
-        }
-        else if (taxBoolean===false){
+        } else if(taxBoolean === false){
             finalPrice = dishData[i]['price']
-        }
-        else{
+        } else {
             console.log("You need to pass a boolean to the getPrices call!")
             return
         }
-        console.log("Dish: " + dishData[i]['name'] + " Price: $" + finalPrice)
+        console.log("Dish: " + dishData[i]['name'] +" Price: $" + finalPrice)
     }
-   
 }
 
 // Implement getDiscount()
 function getDiscount(taxBoolean, guests) {
     getPrices(taxBoolean)
-    
+    if (typeof guests == "number" && guests > 0 && guests < 30){
+        let discount = 0
+        if(guests < 5){
+            discount = 5
+        } else {
+            discount = 10
+        }
+        console.log('Discount is: $' + discount)
+    } else {
+        console.log('The second argument must be a number between 0 and 30')
+    }
 }
-
-// Call getDiscount()
 getDiscount(true, 2)
 getDiscount(false, 10)
